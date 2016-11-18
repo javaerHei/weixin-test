499d772fb306f62066747d48c866f078d9e7efdb
龚明国 <ming-guo.gong@utsoft.cn>
2016-11-18 15:27:02 +0800

    spring session添加

---
+++ src/main/java/com/example/common/Servlets.java
@@ -0,0 +1,100 @@
+package com.example.common;
+
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.Validate;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+/**
+ * Http与Servlet工具类.
+ * @author calvin/thinkgem
+ * @version 2014-8-19
+ */
+public class Servlets {
+
+
+	/**
+	 * 取得带相同前缀的Request Parameters, copy from spring WebUtils.
+	 * 
+	 * 返回的结果的Parameter名已去除前缀.
+	 */
+	@SuppressWarnings("rawtypes")
+	public static Map<String, Object> getParametersStartingWith(ServletRequest request, String prefix) {
+		Validate.notNull(request, "Request must not be null");
+		Enumeration paramNames = request.getParameterNames();
+		Map<String, Object> params = new TreeMap<String, Object>();
+		String pre = prefix;
+		if (pre == null) {
+			pre = "";
+		}
+		while (paramNames != null && paramNames.hasMoreElements()) {
+			String paramName = (String) paramNames.nextElement();
+			if ("".equals(pre) || paramName.startsWith(pre)) {
+				String unprefixed = paramName.substring(pre.length());
+				String[] values = request.getParameterValues(paramName);
+				if (values == null || values.length == 0) {
+					values = new String[]{};
+					// Do nothing, no values found at all.
+				} else if (values.length > 1) {
+					params.put(unprefixed, values);
+				} else {
+					params.put(unprefixed, values[0]);
+				}
+			}
+		}
+		return params;
+	}
+
+	/**
+	 * 组合Parameters生成Query String的Parameter部分,并在paramter name上加上prefix.
+	 * 
+	 */
+	public static String encodeParameterStringWithPrefix(Map<String, Object> params, String prefix) {
+		StringBuilder queryStringBuilder = new StringBuilder();
+
+		String pre = prefix;
+		if (pre == null) {
+			pre = "";
+		}
+		Iterator<Entry<String, Object>> it = params.entrySet().iterator();
+		while (it.hasNext()) {
+			Entry<String, Object> entry = it.next();
+			queryStringBuilder.append(pre).append(entry.getKey()).append("=").append(entry.getValue());
+			if (it.hasNext()) {
+				queryStringBuilder.append("&");
+			}
+		}
+		return queryStringBuilder.toString();
+	}
+
+	/**
+	 * 客户端对Http Basic验证的 Header进行编码.
+	 */
+	public static String encodeHttpBasic(String userName, String password) {
+		String encode = userName + ":" + password;
+		return "Basic " + Encodes.encodeBase64(encode.getBytes());
+	}
+
+	
+	/**
+	 * 获取当前请求对象
+	 * @return
+	 */
+	public static HttpServletRequest getRequest(){
+		try{
+			return ((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getRequest();
+		}catch(Exception e){
+			return null;
+		}
+	}
+
+	
+}
