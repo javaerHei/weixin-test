58fdf6f1c37eadcaa1387e796d4f4852b74b9729
龚明国 <ming-guo.gong@utsoft.cn>
2016-12-15 15:42:21 +0800

    test

--- src/main/java/com/example/SpringbootApplication.java
+++ src/main/java/com/example/SpringbootApplication.java
@@ -3,6 +3,14 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * spring boot
+ * <br>创建日期：2016年12月15日
+ * 
+ * @author gongmingguo
+ * @since 1.0
+ * @version 1.0
+ */
 @SpringBootApplication
 public class SpringbootApplication {
 
